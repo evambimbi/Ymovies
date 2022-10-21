09f3dd72460288241a98811273d4131985118f67
Eveline <evelinembimbi@gmail.com>
2022-10-21 13:24:05 +0100

    responsive parti 1

--- src/components/acceuil/Header.jsx
+++ src/components/acceuil/Header.jsx
@@ -1,51 +1,84 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faYoutube } from "@fortawesome/free-brands-svg-icons";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { faHamburger } from "@fortawesome/free-solid-svg-icons";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { faVideo } from "@fortawesome/free-solid-svg-icons";
 import "./Header.css";
 import { useState } from "react";
-import Search from "../Search";
+import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import { Context } from "../../ContextAccount/Context";
+import { useContext } from "react";
+import { useRef } from "react";
 
 const Haeder = () => {
   const [text, setText] = useState("");
-const navigate=useNavigate();
+  const [menu, setMenu] = useState(false);
+  const navigate = useNavigate();
+  const menu_mobil = useRef();
+  console.log(menu_mobil.current);
+  let { signOut } = useContext(Context);
   const handleSearch = () => {
-    navigate(`/Resultat/${text}`)
-  }
+    navigate(`/Resultat/${text}`);
+  };
 
   const userImg = window.localStorage.getItem("image");
   console.log(userImg);
   return (
-    <div className="haeder">
-      <div className="logo-haeder">
-        <FontAwesomeIcon icon={faYoutube} />
-        <h1 className="haeder-title">Ymovies</h1>
-      </div>
-      <div className="input-recherche">
-        <input
-          id="search-input"
-          type="text"
-          className="recherche"
-          name="search-input"
-          placeholder="Recherche"
-          onChange={(e) => setText(e.target.value)}
-          value={text}
-        />
-        {/* <Link to={`/Resultat/${text}`}> */}
+    <>
+      <div className="haeder">
+        <div className="logo-haeder">
+          <FontAwesomeIcon icon={faYoutube} />
+          <h1 className="haeder-title">Ymovies</h1>
+        </div>
+        <div className="input-recherche">
+          <input
+            id="search-input"
+            type="text"
+            className="recherche"
+            name="search-input"
+            placeholder="Recherche"
+            onChange={(e) => setText(e.target.value)}
+            value={text}
+          />
           <button className="icon-search" onClick={handleSearch}>
             <FontAwesomeIcon icon={faSearch} />
           </button>
-        {/* </Link> */}
-      </div>
-      <div className="profil-user">
-        <div className="profil-icon">
-          <div className="icon-user">
-            <img src={userImg} alt="imageUser" />
+        </div>
+        <button onClick={()=>console}>
+          <FontAwesomeIcon icon={faHamburger} />
+        </button>
+
+        <div className="profil-user">
+          <div className="profil-icon">
+            <div className="icon-user">
+              <img src={userImg} alt="imageUser" />
+            </div>
+            <p className="monCompte">Mon compte</p>
           </div>
-          <p className="monCompte">Mon compte</p>
         </div>
       </div>
-    </div>
+
+      <div ref={menu_mobil} className="Menu-mobil">
+        <Link to={"/dashboard"}>
+          <div className="Menu-title">
+            <FontAwesomeIcon icon={faHome} />
+            <p className="acceuil-mobil">Acceuil</p>
+          </div>
+        </Link>
+        <div className="abonnement-mobil">
+          <FontAwesomeIcon icon={faVideo} />
+          <Link to="/abonnement">
+            <p>Abonnement</p>
+          </Link>
+        </div>
+        <button className="deconnect-mobil" onClick={signOut}>
+          {" "}
+          se déconnecter
+        </button>
+      </div>
+    </>
   );
 };
 
