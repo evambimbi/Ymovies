bbc776e774aa8095216c6a462a004fd66593e901
Eveline <evelinembimbi@gmail.com>
2022-10-26 16:22:11 +0100

    Ajout de hover sur le menu

--- src/components/acceuil/Acceuil.jsx
+++ src/components/acceuil/Acceuil.jsx
@@ -11,29 +11,19 @@ const Acceuil = () => {
   return (
     <div className="acceuil">
       <NavLink to={"/dashboard"}>
-        {/* <div className="acceuil-title"> */}
         <FontAwesomeIcon className="Nav-icon" icon={faHome} />
         Acceuil
-        {/* <p className="acc-title">Acceuil</p> */}
-        {/* </div> */}
       </NavLink>
-      {/* <div className="abonnement"> */}
       <NavLink to="/abonnement">
         <FontAwesomeIcon className="Nav-icon" icon={faVideo} />
         Abonnement
-        {/* <p>Abonnement</p> */}
       </NavLink>
-      {/* </div> */}
-      {/* <div className="videoLike"> */}
       <NavLink to="/videolike">
         <FontAwesomeIcon className="Nav-icon" icon={faThumbsUp} />
         Vidèos "j'aime"
-        {/* <p>Vidèos "j'aime"</p> */}
       </NavLink>
-      {/* </div> */}
       <button className="deconnect" onClick={signOut}>
-        {" "}
-        se déconnecter
+        se dèconnecter
       </button>
     </div>
   );
